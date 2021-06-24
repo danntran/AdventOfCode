3a61528ea9ce32468a126be7f538ce6de5690eb1
danntran <54047713+danntran@users.noreply.github.com>
2021-06-24 12:12:04 +1000

    Alt SOlution to 10

--- 10-1.py
+++ 10-1.py
@@ -1,30 +1,17 @@
-text_file = open("10_input.txt", "r")
+text_file = open("10-input.txt", "r")
 lines = text_file.readlines()
 text_file.close()
 
-lines = list(map(int, lines))
+cleaned_list = []
+for line in lines:
+    # remove newline
+    line_str = line.strip()
+    cleaned_list.append(int(line_str))
 
-lines.sort()
-lines.insert(0, 0)
-lines.append(lines[-1]+3)
+print(cleaned_list)
 
-print (lines)
+cur_jolts = 0
 
-difs_1 = 0
-difs_2 = 0
-difs_3 = 0
-
-for i in range(1, len(lines)):
-    dif = lines[i] - lines[i-1] 
-    if dif == 1:
-        difs_1 += 1
-    elif dif == 2:
-        difs_2 += 1
-    elif dif == 3:
-        difs_3 += 1
-    else:
-        print("Error")
-
-print("1 dif = ", difs_1)
-print("3 dif = ", difs_3)
-print("multiply ", difs_1*difs_3)
+for idx, val in enumerate(cleaned_list):
+    for jolts in cleaned_list:
+        jolts 
